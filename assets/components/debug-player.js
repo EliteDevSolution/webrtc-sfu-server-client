df58833e12b7fc6f96e01c354c2529f193f14e5c
Jasper Lee <jasper.lee@medicoinfotech.com>
2024-03-05 10:08:48 +0900

    assest

--- assets/components/debug-player.js
+++ assets/components/debug-player.js
@@ -3,32 +3,13 @@ export default class VideoSizeSelectElement extends HTMLElement {
         super()
         this.labelCount = document.createElement("label")
         this.labelSize = document.createElement("label")
-        this.video = document.createElement("video")
-        this.video.autoplay = true
-        this.video.controls = true
-
-        // https://github.com/Orphis/webrtc-sandbox/blob/811a6cc548c3e82838dc7a66533460b205716c54/simulcast-playground.html#L1107
-        this.video.style.width = "320px"
+        this.video = document.getElementById("video")
         this.video.onloadedmetadata = this.setShowSize
         this.video.onresize = this.setShowSize
     }
 
     connectedCallback() {
-        const shadow = this.attachShadow({ mode: "closed" })
-
-        const selectWidthValue = document.createElement("select")
-        selectWidthValue.onchange = ev => this.video.style.width = ev.target.value
-
-        const addSelectOptions = (arr, root) => arr.map(i => {
-            const option = document.createElement("option")
-            option.value = i
-            option.innerText = i
-            root.append(option)
-        })
-        addSelectOptions(["320px", "480px", "600px", "1280px", "1920px", "auto"], selectWidthValue)
-
         const labelWidthHeight = document.createElement("label")
-
         shadow.append(labelWidthHeight)
         shadow.append(this.labelCount)
         shadow.append(document.createElement("br"))
